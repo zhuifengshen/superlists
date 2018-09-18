fd6b72a87be8aefdccc4bfb999a46719ffd2bd5b
devin <zhangchuzhao@youmi.net>
2018-09-18 19:24:11 +0800

    test: add new functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,10 +39,11 @@ class NewVisitorTest(LiveServerTestCase):
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 她按回车键后，页面更新了
-        # 待办事项表格中显示了“1: Buy peacock feathers”
+        # 她按回车键后，被带到了一个新URL
+        # 这个页面的待办事项清单中显示了“Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         # table = self.browser.find_element_by_id('id_list_table')  # 元素不存在，则抛出异常
         # rows = table.find_elements_by_tag_name('tr')  # 元素不存在，返回一个空列表
         # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
@@ -69,7 +70,38 @@ class NewVisitorTest(LiveServerTestCase):
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
-        self.fail('Finish the test!')
-
+        # self.fail('Finish the test!')
         # 她访问那个URL，发现她的待办事项列表还在
         # 她很满意，去睡觉了
+
+        # 现在一个叫费朗西斯的新用户访问了网站
+
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不想伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人都很满意，去睡觉了
