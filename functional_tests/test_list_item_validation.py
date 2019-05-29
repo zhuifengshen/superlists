92bd14471e14050ee077231ffbfd9758b05a8e31
devin <zhangchuzhao@youmi.net>
2019-05-29 11:08:56 +0800

    test: Split out unit tests into two files

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,21 +1,32 @@
-from unittest import skip
 from functional_tests.base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-    @skip
+
     def test_cannot_add_empty_list_items(self):
         # 伊迪丝访问首页，不小心提交了一个空白办事项
         # 输入框中没输入内容，她就按下了回车键
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 首页刷新了，显示一个错误信息
         # 提示待办事项不能为空
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 她输入一些文字，然后再次提交，这次没问题了
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # 她有点儿调皮，又提交了一个空白待办事项
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 在清单页面她看到了一个类似的错误消息
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入文字之后就没问题了
-        self.fail('write me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
