9379379e92c7d91f2effc22b29fcc9c39bc3dbe7
devin <zhangchuzhao@youmi.net>
2019-11-14 18:54:22 +0800

    move sqlite database outside of main source tree

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,12 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 class FunctionalTest(StaticLiveServerTestCase):
     """
     从1.4版开始，Django提供了LiveServerTestCase类，它会自动创建一个测试数据库（跟单元测试一样），并启动一个开发服务器，让功能测试在其中运行
+    python3 manage.py test functional_tests
+    or
+    python3 manage.py test functional_tests \ --liveserver=my-super-lists.site
+    or
+    python3 manage.py test functional_tests liveserver=my-super-lists.site
+
     """
     @classmethod
     def setUpClass(cls):
